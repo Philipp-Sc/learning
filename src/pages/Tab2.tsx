8237e2ce9b81d7600505947b4846f227c3b8fc85
Philipp Schlütermann <philipp.schluetermann@udo.edu>
2021-08-09 16:39:12 +0800

    Stockfish added, chess added

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,18 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import AppContainer from '../components/AppContainer';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+    <IonPage> 
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">My Best Ever Training App</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <AppContainer name="Introduction page" />
       </IonContent>
     </IonPage>
   );
