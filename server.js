e5b22932ef9c4c397a440a57c34294b0d901eb65
Philipp Schlütermann <philipp.schluetermann@udo.edu>
2021-08-11 09:43:13 +0200

    rate limiting & maximum requests

--- server.js
+++ server.js
@@ -5,8 +5,21 @@ var fs = require('fs')
 var https = require('https')
 const app = express();
 var cors = require('cors')
+const rateLimit = require("express-rate-limit");
+
+https.globalAgent.maxSockets = 100;
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
  
 app.options('/', cors()) // enable pre-flight request for DELETE request
+
+
+//  apply to all requests
+app.use(limiter);
 
 app.use(function(req, res, next) {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
